e1b9cf539898ef27f0682029dbcd3f1bbaa5aada
Александр Иванов <kvasssukr.net@gmail.com>
2024-09-05 14:31:49 +0300

    0.1.3181

--- components/layout/product/variations/CarouselItem.tsx
+++ components/layout/product/variations/CarouselItem.tsx
@@ -8,18 +8,18 @@ interface VariationProps {
 const CarouselItem: React.FC<VariationProps> = ({ title, imageSrc }) => {
   return (
     <div className="relative box-border flex w-20 shrink-0 flex-col">
-      <article className="flex max-w-[80px] flex-col gap-2 whitespace-nowrap text-center text-sm text-slate-300">
-        <div className="min-h-[93px] w-full bg-neutral-100">
+      <div className="flex max-w-[80px] flex-col gap-1 overflow-hidden whitespace-nowrap text-center text-sm text-slate-300">
+        <div className="h-[80px] w-full bg-neutral-100">
           <Image
-            fill
-            sizes="(min-width: 300px) 66vw, 100vw"
+            width={80}
+            height={80}
             src={imageSrc}
             alt="Product"
-            className="size-full shrink-0 rounded-xl object-cover"
+            className="size-full shrink-0 rounded-lg object-cover"
           />
         </div>
-        <h3 className="w-full leading-4">{title}</h3>
-      </article>
+        <h3 className="w-full text-center leading-4">{title}</h3>
+      </div>
     </div>
   );
 };
--- components/layout/product/variations/VariationsCarousel.tsx
+++ components/layout/product/variations/VariationsCarousel.tsx
@@ -1,47 +1,24 @@
 /* eslint-disable tailwindcss/no-custom-classname */
-'use client';
-
-import '@egjs/react-flicking/dist/flicking.css';
-
-import { Arrow } from '@egjs/flicking-plugins';
-import Flicking, { ViewportSlot } from '@egjs/react-flicking';
-
 import { variationsItems } from '@/components/data';
 
 import CarouselItem from './CarouselItem';
 import NavigationButton from './NavigationButton';
 
 const VariationsCarousel: React.FC = () => {
-  const plugins = [new Arrow()];
-
   return (
-    <nav className="flex w-full items-center justify-center gap-3 self-stretch">
+    <nav className="flex w-full items-center justify-center self-stretch">
       <div className="flex gap-1.5 self-stretch">
-        <Flicking
-          plugins={plugins}
-          // hideBeforeInit={true}
-          // firstPanelSize="200px"
-          align="prev"
-          circular={true}
-          // eslint-disable-next-line @typescript-eslint/no-unused-vars
-          onMoveEnd={(e) => {
-            // console.log(e);
-          }}
-        >
-          {variationsItems.map((item, idx) => (
-            <div key={idx}>
-              <CarouselItem title={item.title} imageSrc={item.imageSrc} />
-            </div>
-          ))}
-          <ViewportSlot>
-            <div className="flicking-arrow-prev is-outside">
-              <NavigationButton direction="left" />
-            </div>
-            <div className="flicking-arrow-next is-outside">
-              <NavigationButton direction="right" />
-            </div>
-          </ViewportSlot>
-        </Flicking>
+        {variationsItems.map((item, idx) => (
+          <CarouselItem key={idx} title={item.title} imageSrc={item.imageSrc} />
+        ))}
+        <div>
+          <div className="flicking-arrow-prev is-outside">
+            <NavigationButton direction="left" />
+          </div>
+          <div className="flicking-arrow-next is-outside">
+            <NavigationButton direction="right" />
+          </div>
+        </div>
       </div>
     </nav>
   );
