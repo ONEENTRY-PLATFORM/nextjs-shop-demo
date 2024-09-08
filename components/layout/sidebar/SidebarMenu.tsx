c16cc4673bcd305ee6f1dd0087763127251ee3fc
Александр Иванов <kvasssukr.net@gmail.com>
2024-09-08 15:22:26 +0300

    0.1.333

--- components/layout/sidebar/SidebarMenu.tsx
+++ components/layout/sidebar/SidebarMenu.tsx
@@ -1,9 +1,6 @@
-'use client';
-
-import { usePathname } from 'next/navigation';
 import type { IMenusPages } from 'oneentry/dist/menus/menusInterfaces';
 
-import { useGetMenu } from '@/app/api';
+import { getMenuByMarker } from '@/app/api/serverSideProps';
 import { SidebarMenuLoader } from '@/components/shared/Loader';
 
 import SidebarMenuItem from './SidebarMenuItem';
@@ -11,27 +8,23 @@ import SidebarMenuItem from './SidebarMenuItem';
 // export const revalidate = 10;
 // export const dynamicParams = true;
 
-export default function SidebarMenu() {
-  const paths = usePathname();
-  const pathNames = paths.split('/').filter((path: unknown) => path);
-  const { menu, loading, error } = useGetMenu({
+export default async function SidebarMenu() {
+  const { isError, menu } = await getMenuByMarker({
     marker: 'side_web',
+    langCode: 'en_US',
   });
 
-  if (error || !menu || loading) {
+  if (isError || !menu) {
     return <SidebarMenuLoader />;
   }
+
   const pages = menu.pages as Array<IMenusPages & { isActive: boolean }>;
 
   return (
     <nav className="flex w-full">
       <ul className="flex w-full flex-row gap-2 overflow-x-auto overflow-y-hidden py-3 text-base text-neutral-600 md:max-w-[165px] md:flex-col md:gap-5 md:overflow-hidden md:py-0">
         {pages.map((item) => {
-          const el = {
-            ...item,
-            isActive: item.pageUrl === pathNames[0],
-          };
-          return <SidebarMenuItem key={item.id} menuItem={el} />;
+          return <SidebarMenuItem key={item.id} menuItem={item} />;
         })}
       </ul>
     </nav>
--- components/layout/sidebar/SidebarMenuItem.tsx
+++ components/layout/sidebar/SidebarMenuItem.tsx
@@ -1,4 +1,7 @@
+'use client';
+
 import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 import type { IMenusPages } from 'oneentry/dist/menus/menusInterfaces';
 
 import * as icons from '../../icons';
@@ -6,21 +9,25 @@ import * as icons from '../../icons';
 const SidebarMenuItem: React.FC<{
   menuItem: IMenusPages & { isActive: boolean };
 }> = ({ menuItem }) => {
+  const paths = usePathname();
+  const pathNames = paths.split('/').filter((path: unknown) => path);
+
   if (!menuItem) {
     return;
   }
   const { pageUrl, localizeInfos } = menuItem;
   const Icon = icons[pageUrl as keyof typeof icons];
+  const isActive = menuItem.pageUrl === pathNames[0];
 
   return (
     <li>
       <Link
-        className={`mr-auto flex justify-start gap-3 whitespace-nowrap pr-5 hover:text-orange-500 ${menuItem.isActive ? 'text-orange-500' : ''} group`}
+        className={`mr-auto flex justify-start gap-3 whitespace-nowrap pr-5 hover:text-orange-500 ${isActive ? 'text-orange-500' : ''} group`}
         href={'/' + pageUrl}
-        aria-checked={menuItem.isActive}
+        aria-checked={isActive}
       >
         <div className="my-auto aspect-square size-4 shrink-0">
-          <Icon active={menuItem.isActive} />
+          <Icon active={isActive} />
         </div>
         <div>{localizeInfos.menuTitle}</div>
       </Link>
