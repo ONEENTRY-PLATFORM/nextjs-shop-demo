eb097167f0cc6a2357871b785b16ad48872c3903
Александр Иванов <kvasssukr.net@gmail.com>
2024-09-18 19:10:18 +0300

    0.1.346

--- components/layout/home/CatalogCard.tsx
+++ components/layout/home/CatalogCard.tsx
@@ -16,30 +16,69 @@ const CatalogCard: FC<CatalogCardProps> = async ({ cardData }) => {
     langCode: 'en_US',
   });
 
-  if (!block?.attributeValues || !block.isVisible || isError) {
+  const classNames = {
+    home_banner: {
+      class_name: 'bg-amber-600',
+      card_width: 'w-full',
+      card_height: 'h-[175px]',
+    },
+    offer_best_seller: {
+      class_name: 'bg-purple-600',
+      card_width:
+        'w-full lg:w-[calc(_33%_-_0.65rem)] md:w-[calc(_50%_-_0.65rem)]',
+      card_height: 'h-[260px]',
+    },
+    offer_promotion: {
+      class_name: 'bg-blue-500',
+      card_width:
+        'w-full lg:w-[calc(_33%_-_0.65rem)] md:w-[calc(_50%_-_0.65rem)]',
+      card_height: 'h-[260px]',
+    },
+    offer_offer_day: {
+      class_name: 'bg-lime-700',
+      card_width:
+        'w-full lg:w-[calc(_33%_-_0.65rem)] md:w-[calc(_50%_-_0.65rem)]',
+      card_height: 'h-[260px]',
+    },
+    offer_new_arrivals: {
+      class_name: 'bg-teal-300',
+      card_width: 'w-full md:w-[calc(_50%_-_0.65rem)]',
+      card_height: 'h-[260px]',
+    },
+    offer_youtube: {
+      class_name: 'bg-amber-300',
+      card_width: 'w-full lg:w-[calc(_50%_-_0.65rem)]',
+      card_height: 'h-[260px]',
+    },
+  };
+
+  if (!block || !block.attributeValues || !block.isVisible || isError) {
     return null;
   }
 
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const { title, class_name, card_width, card_height, bg_web, link } =
     block.attributeValues;
   const imageSrc = bg_web?.value[0]?.downloadLink || '/images/placeholder.jpg';
   const sticker = block.attributeValues.stickers;
   const quote = block.attributeValues.quote?.value || '';
 
+  const className = classNames[cardData as keyof typeof classNames];
+
   return (
     <Link
       href={'shop/' + link?.value || ''}
-      className={`relative flex flex-col ${card_width?.value} ${card_height?.value} grow flex-col justify-center text-2xl font-bold text-white`}
+      className={`relative flex flex-col ${className.card_width} ${className.card_height} grow flex-col justify-center text-2xl font-bold text-white`}
     >
       <div
-        className={`relative flex size-full p-6 ${class_name?.value} overflow-hidden rounded-3xl`}
+        className={`relative flex size-full p-6 ${className.class_name} overflow-hidden rounded-3xl`}
       >
         {sticker?.value[0] ? (
           <div className="absolute left-3 top-3 z-10">
             <Image
               width={30}
               height={30}
-              src={sticker?.value[0]?.extended?.value.downloadLink}
+              src={sticker.value[0].extended?.value.downloadLink}
               alt={''}
             />
           </div>
--- components/layout/home/CatalogGrid.tsx
+++ components/layout/home/CatalogGrid.tsx
@@ -10,7 +10,6 @@ const CatalogGrid: FC<CatalogGridProps> = async ({ blocks }) => {
   if (blocks?.length < 1) {
     return;
   }
-  console.log(blocks);
 
   return (
     <div className="flex w-full flex-wrap justify-between gap-5 max-md:flex-col">
