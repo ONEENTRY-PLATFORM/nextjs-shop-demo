5b26c4ba2d81b19cefc673aa746123b745cd2682
Александр Иванов <kvasssukr.net@gmail.com>
2024-11-05 12:55:57 +0200

    0.2.82

--- app/api/api/api.ts
+++ app/api/api/api.ts
@@ -1,7 +1,8 @@
 import { defineOneEntry } from 'oneentry';
 
-const PROJECT_URL = process.env.NEXT_PUBLIC_PROJECT_URL as string;
-const APP_TOKEN = process.env.NEXT_PUBLIC_APP_TOKEN as string;
+const PROJECT_URL = 'https://react-native-course.oneentry.cloud';
+const APP_TOKEN =
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoicmVhY3RfYXBwIiwic2VyaWFsTnVtYmVyIjoxLCJpYXQiOjE3MDA0ODAwMDYsImV4cCI6MTc0Nzk5OTk2MX0.gz3KTCITg6FhM_SwtuOZl3GsMr4MlVEPg9sw3d8Q0Po';
 
 // This function used to update user JWT token
 const saveFunction = async (refreshToken: string) => {
