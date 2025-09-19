fa40ef5c57c00c860347796e100f4899826facb2
Alexandr <kvasssukr.net@gmail.com>
2025-09-19 09:51:27 +0300

    0.3.633

--- components/layout/footer/components/MenuItem.tsx
+++ components/layout/footer/components/MenuItem.tsx
@@ -30,7 +30,7 @@ const MenuItem: FC<MenuItemProps> = ({ page, lang }) => {
   return (
     <li className="relative box-border">
       <Link
-        prefetch={true}
+        prefetch={false}
         className={
           'hover:text-red-500 font-normal ' + (isActive ? 'text-red-500' : '')
         }
