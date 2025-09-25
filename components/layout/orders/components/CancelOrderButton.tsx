0d078760759ccff3abc9fdbe052a0124b3b5660f
Alexandr <kvasssukr.net@gmail.com>
2025-09-25 04:03:21 +0300

    0.3.670

--- components/layout/orders/components/CancelOrderButton.tsx
+++ components/layout/orders/components/CancelOrderButton.tsx
@@ -2,6 +2,7 @@
 import type {
   IOrderByMarkerEntity,
   IOrderData,
+  IOrderProducts,
 } from 'oneentry/dist/orders/ordersInterfaces';
 import type { FC } from 'react';
 
@@ -35,7 +36,8 @@ const CancelOrderButton: FC<CancelOrderButtonProps> = ({
   const cancelOrderHandle = async () => {
     const formData = {
       ...data,
-      products: data.products.map((product) => ({
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      products: data.products?.map((product: any) => ({
         productId: product.id,
         quantity: product.quantity,
       })),
